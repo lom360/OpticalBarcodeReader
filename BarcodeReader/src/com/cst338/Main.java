c7d9b7e3e8c7d318e242bbd39a247ca0dd228a9b
Lom Thin <sarom_thin@outlook.com>
2023-03-31 01:03:16 -0700

    Started on the Barcode Assignment

---
+++ BarcodeReader/src/com/cst338/Main.java
@@ -0,0 +1,13 @@
+package com.cst338;
+
+interface BarcodeIO {
+	public void doSomething();
+}
+
+
+
+public class Main {
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ BarcodeReader/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Lom
+ *
+ */
+module BarcodeReader {
+}

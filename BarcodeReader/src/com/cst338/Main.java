9122c8dd759ba0dccccecc6b8478db0b5bc2f3bb
Lom Thin <sarom_thin@outlook.com>
2023-03-31 01:26:51 -0700

    Completed boilerplate for BarcodeIO interface, BarcodeImage class, and DataMatrix class.

--- BarcodeReader/src/com/cst338/Main.java
+++ BarcodeReader/src/com/cst338/Main.java
@@ -1,13 +1,44 @@
 package com.cst338;
 
 interface BarcodeIO {
-	public void doSomething();
+	public boolean scan(BarcodeImage bc);
+	public boolean readText(String text);
+	public boolean generateImageFromText();
+	public boolean translateImageToText();
+	public boolean displayTextToConsole();
+	public void displayImageToConsole();
 }
 
+class BarcodeImage implements Cloneable {
+
+}
+
+class DataMatrix implements BarcodeIO {
+	public boolean scan(BarcodeImage bc) {
+		return true;
+	}
+	public boolean readText(String text) {
+		return true;
+	}
+	public boolean generateImageFromText() {
+		return true;
+	}
+	public boolean translateImageToText() {
+		return true;
+	}
+	public boolean displayTextToConsole() {
+		return true;
+	}
+	public void displayImageToConsole() {
+		
+	}
+}
 
 
 public class Main {
 	public static void main(String[] args) {
+		DataMatrix test = new DataMatrix();
 		
+		System.out.println(test.scan(null));
 	}
 }
